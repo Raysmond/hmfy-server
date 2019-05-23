2832160077c5c0409e0ab80a209638521062d09a
Raysmond <jiankunlei@126.com>
2019-05-24 01:31:13 +0800

    add wechat mini app configuration for test

--- src/main/java/com/shield/config/ApplicationProperties.java
+++ src/main/java/com/shield/config/ApplicationProperties.java
@@ -1,6 +1,9 @@
 package com.shield.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
 
 /**
  * Properties specific to Shield.
@@ -9,6 +12,36 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+@Data
 public class ApplicationProperties {
 
+    private List<MiniAppConfig> miniApps;
+
+    @Data
+    public static class MiniAppConfig {
+        /**
+         * 设置微信小程序的appid
+         */
+        private String appid;
+
+        /**
+         * 设置微信小程序的Secret
+         */
+        private String secret;
+
+        /**
+         * 设置微信小程序消息服务器配置的token
+         */
+        private String token;
+
+        /**
+         * 设置微信小程序消息服务器配置的EncodingAESKey
+         */
+        private String aesKey;
+
+        /**
+         * 消息格式，XML或者JSON
+         */
+        private String msgDataFormat;
+    }
 }
